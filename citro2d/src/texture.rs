ed089303b36e7d44eb62f75eec1bbd7e39c8fca2
Samuel Kyletoft <samuel@kyletoft.se>
2026-02-04 22:53:34 +0100

    citro2d/sprites: Remove unused imports

--- citro2d/src/texture.rs
+++ citro2d/src/texture.rs
@@ -1,7 +1,3 @@
-use citro2d_sys::{
-    C2D_DrawParams, C2D_DrawParams__bindgen_ty_1, C2D_DrawParams__bindgen_ty_2, C2D_DrawSprite,
-    C2D_Image, C2D_Sprite,
-};
 use citro3d::texture::ColorFormat;
 use citro3d_sys::{C3D_Tex, C3D_TexInit};
 
