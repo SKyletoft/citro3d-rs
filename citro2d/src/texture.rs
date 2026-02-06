a4de972cb08848ecacf5a906ffeb8669ef578df6
Samuel Kyletoft <samuel@kyletoft.se>
2026-02-06 01:54:11 +0100

    citro2d/texture: Fix swizzling

--- citro2d/src/texture.rs
+++ citro2d/src/texture.rs
@@ -28,7 +28,9 @@ impl Tex {
         let fmt = self.0._bitfield_1.get(0, 4) as u8;
         let fmt = ColourFormat::try_from(fmt).unwrap();
 
-        swizzle(&mut texture);
+        let mut texture = texture.clone();
+        swizzle::<T, M, N, O>(&mut texture);
+
         unsafe {
             citro3d_sys::C3D_TexUpload(&raw mut self.0, texture.as_ptr() as *const std::ffi::c_void)
         };
@@ -61,14 +63,50 @@ fn bytes_per_pixel(fmt: ColourFormat) -> usize {
     }
 }
 
-    for y in 0..M {
-        for x in 0..N {
-            // swizzle_block([x, x + 1, x + 2, x + 3], data);
-        }
 fn swizzle<T: Copy + Default, const M: usize, const N: usize, const O: usize>(
     data: &mut [[T; M]; N],
 ) {
+    debug_assert_eq!(M % 8, 0);
+    debug_assert_eq!(N % 8, 0);
+    debug_assert_eq!(M * N, O);
+
+    // Check assembly, is this initialisation optimised out or do we need to use some MaybeUninit hack?
+    let mut out = [Default::default(); O];
+
+    for ((x, y), dst) in (0..N)
+        .step_by(8)
+        .flat_map(|n| (0..M).step_by(8).map(move |m| (m, n)))
+        .flat_map(move |(x, y)| {
+            SWIZZLE_ORDER_2D
+                .iter()
+                .copied()
+                .map(move |(dx, dy)| (x + dx, dy + y))
+        })
+        .zip(out.iter_mut())
+    {
+        *dst = data[x][y]
     }
+
+    let data = unsafe { std::mem::transmute::<&mut [[T; M]; N], &mut [T; O]>(data) };
+    *data = out;
 }
 
-
+#[rustfmt::skip]
+const SWIZZLE_ORDER_2D: [(usize, usize); 64] = [
+    (0,0), (1,0), (0,1), (1,1),
+    (2,0), (3,0), (2,1), (3,1),
+    (0,2), (1,2), (0,3), (1,3),
+    (2,2), (3,2), (2,3), (3,3),
+    (4,0), (5,0), (4,1), (5,1),
+    (6,0), (7,0), (6,1), (7,1),
+    (4,2), (5,2), (4,3), (5,3),
+    (6,2), (7,2), (6,3), (7,3),
+    (0,4), (1,4), (0,5), (1,5),
+    (2,4), (3,4), (2,5), (3,5),
+    (0,6), (1,6), (0,7), (1,7),
+    (2,6), (3,6), (2,7), (3,7),
+    (4,4), (5,4), (4,5), (5,5),
+    (6,4), (7,4), (6,5), (7,5),
+    (4,6), (5,6), (4,7), (5,7),
+    (6,6), (7,6), (6,7), (7,7),
+];
