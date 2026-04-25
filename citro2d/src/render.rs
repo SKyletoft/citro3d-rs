f1c2e2697eb523258641b94c2732c696f6fb4b0f
Samuel Kyletoft <samuel@kyletoft.se>
2026-04-25 03:06:15 +0200

    repo: Format

--- citro2d/src/render.rs
+++ citro2d/src/render.rs
@@ -3,7 +3,7 @@ use std::cell::RefMut;
 
 use ctru::services::gfx::Screen;
 
-use crate::{shapes::Shape, Error, Result};
+use crate::{Error, Result, shapes::Shape};
 
 /// A color in RGBA format. The color is stored as a 32-bit integer
 #[derive(Debug, Clone, Copy, Default)]
--- citro3d/src/uniform.rs
+++ citro3d/src/uniform.rs
@@ -4,7 +4,7 @@
 use std::ops::Range;
 
 use crate::math::{FVec4, IVec, Matrix4};
-use crate::{shader, Frame};
+use crate::{Frame, shader};
 
 /// The index of a uniform within a [`shader::Program`].
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
